866a9fc63c464ef6404860469677416273ee5cdb
Riccardo <riccardoforteleoni@hotmail.it>
2019-04-17 12:27:12 +0200

    prima parte progetto

--- code/js/app.js
+++ code/js/app.js
@@ -1,19 +1,27 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.onSuccess)
-		.fail(app.onError);
+			.done(app.onSuccess)
+			.fail(app.onError);
 	},
 	onSuccess(dataJson) {
 		console.log(dataJson);
+		dataJson.profList.forEach(element => {
+			let html = `<tr>
+			<td>${element.nome}</td>
+			<td>${element.cognome}</td>
+			<td>${element.materia}</td>
+		  </tr>`;
+			$("#container").append(html);
+		});
 
-		
+
 		// Update the table
 		$('#dataTable').DataTable();
-		
+
 	},
-	onError: function(e) {
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
