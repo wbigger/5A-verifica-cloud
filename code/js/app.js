9f9681ff500a4c2c870bd2e56e50b6957a6ccd5f
wbigger <wbigger@gmail.com>
2019-04-27 10:35:04 +0200

    Correzione docente

--- code/js/app.js
+++ code/js/app.js
@@ -1,26 +1,28 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.onSuccess)
-		.fail(app.onError);
+			.done(app.onSuccess)
+			.fail(app.onError);
 	},
 	onSuccess(dataJson) {
 		console.log(dataJson);
 		dataJson.schoolList.forEach(element => {
-			let html=`
+			let html = `
 			<tr>
 			<td>${element.nome}</td>
 			<td>${element.cognome}</td>
 			<td>${element.materia}</td>
-		  </tr>`;
-		  // Update the table
-		$('#dataTable').append(html);
+		  	</tr>`;
+			// Update the table
+			// PROF: NON #dataTable ma "tbody" (o altro identificativo su tbody)
+			$('#dataTable').append(html);
 		});
-		
-		
+
+		// PROF: non dovevi cancellare la riga per il refresh della tabella
+
 	},
-	onError: function(e) {
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
