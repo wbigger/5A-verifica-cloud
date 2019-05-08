1c0c7f7398b8146077f6a77c128dc2798a947cbc
wbigger <wbigger@gmail.com>
2019-05-08 12:14:12 +0200

    Correzione compito

--- code/js/app.js
+++ code/js/app.js
@@ -1,19 +1,28 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.onSuccess)
-		.fail(app.onError);
+			.done(app.onSuccess)
+			.fail(app.onError);
 	},
-	onSuccess(dataJson) {
-		console.log(dataJson);
+	onSuccess(dataJason) {
+		console.log(dataJason);
+		dataJason.commissione.forEach(element => {
+			let html = `
+		<tr>
+		<td>${element.nome}</td>
+		<td>${element.cognome}</td>
+		<td>${element.materia}</td>
+		</tr>
+		`;
+			$("#dataTable").append(html);
+		});
 
-		
-		// Update the table
-		$('#dataTable').DataTable();
-		
+
+
+
 	},
-	onError: function(e) {
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
